bd4f7b546eeacd73c259979688e4c67c2204a95b
bradford lamson-scribner <brad.lamson@gmail.com>
2024-01-15 08:32:48 -0700

    update for server and stale message ticker to gracefully shut down

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/cipherbin/cipher-bin-server/internal/app"
@@ -28,24 +31,62 @@ func main() {
 	}
 	defer db.Close()
 
-	srv := app.NewServer(db)
-
-	// Spin off go routine that checks every 5 minutes for stale messages.
-	// If a message is 30 days or older, it will be destroyed.
-	go func() {
-		uptimeTicker := time.NewTicker(5 * time.Minute)
-		defer uptimeTicker.Stop()
-
-		for range uptimeTicker.C {
-			srv.Db.DestroyStaleMessages()
-		}
-	}()
+	appSrv := app.NewServer(db)
 
 	port := os.Getenv("CIPHER_BIN_PORT")
 	if port == "" {
 		port = "4000"
 	}
 
+	// Implement a graceful shutdown. This allows in-flight requests to complete before
+	// shutting down the server, preventing potential data loss or corruption.
+	httpServer := &http.Server{Addr: ":" + port, Handler: appSrv.Mux}
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	stopTicker := make(chan struct{})
+
+	go func() {
+		<-sig
+
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 5*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out, forcing exit")
+			}
+		}()
+
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			log.Fatal(err.Error())
+		}
+
+		close(stopTicker)
+		serverStopCtx()
+	}()
+
+	// Spin off go routine that checks every 5 minutes for stale messages.
+	// If a message is 30 days or older, it will be destroyed.
+	go func() {
+		uptimeTicker := time.NewTicker(5 * time.Minute)
+		for {
+			select {
+			case <-uptimeTicker.C:
+				appSrv.Db.DestroyStaleMessages()
+			case <-stopTicker:
+				uptimeTicker.Stop()
+				return
+			}
+		}
+	}()
+
 	fmt.Printf("Serving application on port %s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), srv.Mux))
+
+	err = httpServer.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err.Error())
+	}
+
+	<-serverCtx.Done()
 }
